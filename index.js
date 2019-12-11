984197e7637910f0e860ba49794eb53fa2c6e39a
Toledo <gustavo@obex.pw>
2019-12-11 17:49:48 -0300

    .

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import Biscoint from "biscoint-api-node";
 
 const bc = new Biscoint({
   apiKey: "qwerty",
-  apiSecret: "qwerty"
+  apiSecret: "qwerty",
+  apiUrl: "http://localhost:3000"
 });
 
 const parameters = {
   amount: 50,
   base: "BRL",
-  initialBuy: true
+  initialBuy: true,
+  minProfitPercent: 0.1,
+  retry: 1
 };
 
 setInterval(async () => {
@@ -18,20 +21,31 @@ setInterval(async () => {
       base: parameters.base,
       op: "buy"
     });
-    const sellOffer = await bc.offer({
-      amount: parameters.amount,
-      base: parameters.base,
-      op: "sell"
-    });
-    if (buyOffer.quote < sellOffer.quote) {
+
+    const sellOffer = retry(
+      bc.offer,
+      {
+        amount: parameters.amount,
+        base: parameters.base,
+        op: "sell"
+      },
+      parameters.retry
+    );
+    const profit = percent(buyOffer.quoteamount, sellOffer.quoteamount);
+    if (
+      profit >
+      parameters.minProfitPercent
+    ) {
       try {
         if (parameters.initialBuy) {
           const confirmedBuy = await bc.confirmOffer({
             offerId: buyOffer.offerId
           });
+
           const confirmedSell = await bc.confirmOffer({
             offerId: sellOffer.offerId
           });
+
         } else {
           const confirmedSell = await bc.confirmOffer({
             offerId: sellOffer.offerId
@@ -40,6 +54,8 @@ setInterval(async () => {
             offerId: buyOffer.offerId
           });
         }
+
+        console.log(`Success, profit: + ${profit}`);
       } catch (error) {
         console.log("Error on confirm offer");
         console.log(error);
@@ -50,3 +66,24 @@ setInterval(async () => {
     console.error(error);
   }
 }, 5 * 1000);
+
+function percent(value1, value2) {
+  return (percent = Number(value2) / Number(value1) - 1);
+}
+
+function retry(cb, config, tryes) {
+  return new Promise(async (resolve, reject) => {
+    for (let i = 0; i <= tryes; i++) {
+      try {
+        resolve(cb(config));
+      } catch (error) {
+        if (i >= tryes) reject(error);
+        await sleep(500);
+      }
+    }
+  });
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve(), ms));
+}
