b0e0e9829ac75858f5b9ccd05d4910c69023eb60
Toledo <gustavo@obex.pw>
2019-12-30 18:46:12 -0300

    update api version

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export default {
   apiKey: "8e2e63f69905b2d3b335bcd14b171bd0a6a6ec596804e4e693864f3fd145406d",
   apiSecret: "d33e37c496d58d70d22a0208fe5b937ad7a1af06d43af6409045ff0a111ae922",
-  amount: 0.1,
-  base: "BTC",
+  amount: 100 ,
+  base: "BRL",
   initialBuy: true,
   minProfitPercent: 0.01
 };
